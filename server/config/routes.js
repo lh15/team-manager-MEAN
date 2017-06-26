4cf6bdb200911eef66111eb87a8e320ed85fe8d8
Leibelhecht <leibelhecht@gmail.com>
2017-06-25 20:48:58 -0500

    blah

--- server/config/routes.js
+++ server/config/routes.js
@@ -4,6 +4,7 @@ var Player = mongoose.model('Player');
 var players = require('../controllers/players.js');
 module.exports = function (app) {
     app.get('/api', players.index);
+    app.get('/', players.index);
     app.post("/api/player", players.player);
     app.post("/api/update_player/:id", players.update_player);
     app.delete("/api/delete_player/:id", players.delete_player);
